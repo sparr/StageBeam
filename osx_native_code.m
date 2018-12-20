286ae27c3dab8896049769fce16fcf02c974a180
Clarence "Sparr" Risher <sparr0@gmail.com>
2018-12-19 21:54:51 -0800

    resolve linker problem with `extern "C" {}`, now Cocoa problem

--- osx_native_code.m
+++ osx_native_code.m
@@ -3,6 +3,5 @@
 #include "osx_native_code.h"
 
 void NSRunningApplication_setPresentationOptions(unsigned int options) {
-    NSApplication *this = (NSApplication *) NSRunningApplication.currentApplication;
-    this.presentationOptions = options;
+    ((NSApplication *) NSRunningApplication.currentApplication).presentationOptions = options;
 }
